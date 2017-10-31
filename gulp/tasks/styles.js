98ade202b55a87b3e8bf21171ebc0ec0b95f2867
Thales <thalesburque@gmail.com>
2017-10-30 22:21:50 -0300

    learning to style

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer')
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+postcss = require('gulp-postcss'), //player for other tools
+autoprefixer = require('autoprefixer') // autoprefixer css code
+cssvars = require('postcss-simple-vars'), //allows creation of css variables
+nested = require('postcss-nested'), //nest css code to any browser
+cssImport = require('postcss-import'), //allows css classes to be imported
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
+  return gulp.src('./app/assets/styles/styles.css') //import from this css file
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer])) //allows these variables to run
+    .on('error', function(errorInfo){
+      console.log(errorInfo.toString()); //display error message
+      this.emit('end'); //if an error accurs, watch task doesn't stop running
+    })
+    .pipe(gulp.dest('./app/temp/styles')); // to a new css file
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,20 +1,23 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch'),
-browserSync = require('browser-sync').create();
+watch = require('gulp-watch'), //watchs for some change
+browserSync = require('browser-sync').create(); //update browser when any change accurs
 
 gulp.task('watch', function () {
 
+  // creates a server to run the page
   browserSync.init({
-    notify: false,
+    notify: false, //doesn't show in the browser a popup informing some change
     server: {
       baseDir: "app"
     }
   });
 
+  //when index.html is saved browser updates by itself
   watch('./app/index.html', function () {
     browserSync.reload();
   });
 
+  //watch for css changes
   watch('./app/assets/styles/**/*.css', function () {
     gulp.start('cssInject');
   });
